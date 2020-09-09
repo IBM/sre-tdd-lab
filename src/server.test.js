31a70b5bf25f498bbdf5b689f0ae8010462b9b96
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-09 13:21:31 -0400

    test(server): red - test the full instantiation of the server

--- src/server.test.js
+++ src/server.test.js
@@ -9,11 +9,19 @@ const expressModuleStub = sinon.stub();
 const serverMock = proxyquire('./server', {
     express: expressModuleStub.returns(expressMock)
 });
+const consoleSpy = sinon.spy(global.console, 'log');
 
 tap.test('start an express server', t => {
     serverMock.start();
 
     t.ok(expressModuleStub.calledOnceWith(), 'server instantiates');
+    t.ok(expressMock.listen.calledOnceWith(3000, sinon.match.func), 'server starts');
+
+    const appListenCallback = expressMock.listen.getCall(0).args[1];
+
+    appListenCallback();
+
+    t.ok(consoleSpy.calledWith('The server is running on port 3000.'), 'server startup log message');
 
     t.end();
 });
