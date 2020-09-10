15fa587a56288e46afba632c6de7177be7688022
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-09 23:25:04 -0400

    test(jokes): red - error case for get random joke

--- src/resources/dad-jokes.test.js
+++ src/resources/dad-jokes.test.js
@@ -6,12 +6,30 @@ const got = require('got');
 
 const {BASIC_HEADERS, DAD_JOKES_API_BASE_URL} = require('./constants');
 
+const bodyMock = {
+    some: 'response'
+};
+
 const getStub = sinon.stub(got, 'get');
 
-tap.test('get a random dad joke', t => {
-    dadJokes.getRandomJoke();
+tap.test('get a random dad joke', async t => {
+    getStub.resolves({
+        body: bodyMock
+    });
+
+    const response = await dadJokes.getRandomJoke();
 
     t.ok(getStub.calledOnceWith(DAD_JOKES_API_BASE_URL, BASIC_HEADERS), 'get call for a random dad joke fires');
+    t.equal(response, bodyMock, 'returns the response body');
+
+    t.end();
+});
+
+tap.test('when the call to get a random dad joke fails', t => {
+    const errorMock = 'some error';
+    getStub.rejects(errorMock);
+
+    t.rejects(() => dadJokes.getRandomJoke(), new Error(errorMock), 'throws the error');
 
     t.end();
 });
