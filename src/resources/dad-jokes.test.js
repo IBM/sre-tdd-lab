7844d22edc0775069bef991f429e689f331aea2b
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-09 22:08:01 -0400

    test(jokes): red - fix unit test
    - the afterEach was breaking the test for some reason - will look into it during refactoring

--- src/resources/dad-jokes.test.js
+++ src/resources/dad-jokes.test.js
@@ -1,19 +1,17 @@
-const dadJokes = require('./data-jokes');
+const dadJokes = require('./dad-jokes');
 
 const tap = require('tap');
 const sinon = require('sinon');
 const got = require('got');
 
-const constants = require('./constants');
+const {DAD_JOKES_API_BASE_URL} = require('./constants');
 
 const getStub = sinon.stub(got, 'get');
 
-tap.afterEach(() => {
-    getStub.restore();
-});
-
-tap.test('random dad joke', t => {
+tap.test('get a random dad joke', t => {
     dadJokes.getRandomJoke();
 
-    t.ok(getStub.calledOnceWith(constants.DAD_JOKES_API_BASE_URL), 'server instantiates');
+    t.ok(getStub.calledOnceWith(DAD_JOKES_API_BASE_URL), 'server instantiates');
+
+    t.end();
 });
