1b8950429c6caa7c3af60686850cc0280aa76c2f
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-09 22:14:36 -0400

    refactor(server): split up the server tests

--- src/server.test.js
+++ src/server.test.js
@@ -17,8 +17,13 @@ tap.test('start an express server', t => {
     t.ok(expressModuleStub.calledOnceWith(), 'server instantiates');
     t.ok(expressMock.listen.calledOnceWith(3000, sinon.match.func), 'server starts');
 
-    const appListenCallback = expressMock.listen.getCall(0).args[1];
+    t.end();
+});
 
+tap.test('when the server starts', t => {
+    serverMock.start();
+
+    const appListenCallback = expressMock.listen.getCall(0).args[1];
     appListenCallback();
 
     t.ok(consoleSpy.calledWith('The server is running on port 3000.'), 'server startup log message');
