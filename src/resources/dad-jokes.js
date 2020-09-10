ca8981b3623cfbb6e45b54c0f4a72cf1789dba5a
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-09 22:27:27 -0400

    fix(jokes): add headers to call

--- src/resources/dad-jokes.js
+++ src/resources/dad-jokes.js
@@ -1,9 +1,9 @@
 const got = require('got');
 
-const {DAD_JOKES_API_BASE_URL} = require('./constants');
+const {BASIC_HEADERS, DAD_JOKES_API_BASE_URL} = require('./constants');
 
 const getRandomJoke = () => {
-    return got.get(DAD_JOKES_API_BASE_URL);
+    return got.get(DAD_JOKES_API_BASE_URL, BASIC_HEADERS);
 };
 
 module.exports = {
--- src/resources/dad-jokes.test.js
+++ src/resources/dad-jokes.test.js
@@ -4,14 +4,14 @@ const tap = require('tap');
 const sinon = require('sinon');
 const got = require('got');
 
-const {DAD_JOKES_API_BASE_URL} = require('./constants');
+const {BASIC_HEADERS, DAD_JOKES_API_BASE_URL} = require('./constants');
 
 const getStub = sinon.stub(got, 'get');
 
 tap.test('get a random dad joke', t => {
     dadJokes.getRandomJoke();
 
-    t.ok(getStub.calledOnceWith(DAD_JOKES_API_BASE_URL), 'server instantiates');
+    t.ok(getStub.calledOnceWith(DAD_JOKES_API_BASE_URL, BASIC_HEADERS), 'server instantiates');
 
     t.end();
 });
