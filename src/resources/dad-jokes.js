11d2271f73573e15a542379ef5ccf1fe844e98e1
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-09 23:25:27 -0400

    feat(jokes): green - add error handling for getting a random joke

--- src/resources/dad-jokes.js
+++ src/resources/dad-jokes.js
@@ -2,8 +2,14 @@ const got = require('got');
 
 const {BASIC_HEADERS, DAD_JOKES_API_BASE_URL} = require('./constants');
 
-const getRandomJoke = () => {
-    return got.get(DAD_JOKES_API_BASE_URL, BASIC_HEADERS);
+const getRandomJoke = async () => {
+    try {
+        const response = await got.get(DAD_JOKES_API_BASE_URL, BASIC_HEADERS);
+
+        return response.body;
+    } catch (error) {
+        throw new Error(error);
+    }
 };
 
 module.exports = {
