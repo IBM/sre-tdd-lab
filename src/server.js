db34a8ee2a1cead932c1192c9e97307e5ed8a574
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-12 10:18:08 -0400

    feat(server): green - configure the routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const routes = require('./routes');
 
 const PORT = 3000;
 
 const start = () => {
     const app = express();
+
+    routes.configureRoutes(app);
 
     app.listen(PORT, () => {
         console.info(`The server is running on port ${PORT}.`);
