f92468cd35917968de1b09a1c84321aad97fe65a
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-12 10:39:53 -0400

    test(server): red - add morgan for logging

--- tests/src/server.test.js
+++ tests/src/server.test.js
@@ -3,15 +3,20 @@ const tap = require('tap');
 const sinon = require('sinon');
 
 const expressMock = {
-    listen: sinon.stub()
+    listen: sinon.stub(),
+    use: sinon.stub()
 };
 const expressModuleStub = sinon.stub();
 const routes = require('../../src/routes');
 
+const morganMock = {};
+const morganModuleStub = sinon.stub();
+
 // In cases where the dependency you are consuming is exported so that no method is visible, you may need to mock it.
 // In these cases Sinon cannot mock it properly. Tools like proxyquire or jest.mock() are helpful.
 const serverMock = proxyquire('../../src/server', {
-    express: expressModuleStub.returns(expressMock)
+    express: expressModuleStub.returns(expressMock),
+    morgan: morganModuleStub.returns(morganMock)
 });
 
 const consoleStub = sinon.stub(global.console, 'info');
@@ -31,6 +36,14 @@ tap.test('configure the server', t => {
 
     t.ok(expressModuleStub.calledOnceWith(), 'server instantiates');
     t.ok(expressMock.listen.calledOnceWith(3000, sinon.match.func), 'server starts');
+
+    t.end();
+});
+
+tap.test('configure the logger', t => {
+    serverMock.start();
+
+    t.ok(expressMock.use.calledWith(morganMock), 'express uses morgan');
 
     t.end();
 });
