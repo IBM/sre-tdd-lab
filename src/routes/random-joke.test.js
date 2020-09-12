893e52712dd521a085b5d5e3295458061a26411d
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-12 07:38:03 -0400

    test(jokes): red - call the random joke resource

--- src/routes/random-joke.test.js
+++ src/routes/random-joke.test.js
@@ -1,17 +1,13 @@
-const {getJoke} = require('./random-joke');
+const {
+    getJoke
+} = require('./random-joke');
 
 const tap = require('tap');
 const sinon = require('sinon');
 
 const dadJokesResources = require('../resources/dad-jokes');
 
-const getRandomJokeStub = sinon.stub(dadJokesResources, 'getRandomJoke');
-
-tap.afterEach(() => {
-    getRandomJokeStub.reset();
-});
-
-tap.test('/random-joke route calls the random joke resource', t => {
+tap.test('/random-joke route exists', t => {
     const appMock = {
         get: sinon.stub()
     };
@@ -20,7 +16,27 @@ tap.test('/random-joke route calls the random joke resource', t => {
 
     t.ok(appMock.get.calledOnceWithExactly('/random-joke', sinon.match.func), 'the dad jokes end-point should be exposed');
 
-    t.ok(getRandomJokeStub.calledOnce, 'get random joke resource called');
+    t.end();
+});
+
+tap.test('calls the random joke resource and returns the body', t => {
+    const getRandomJokeStub = sinon.stub(dadJokesResources, 'getRandomJoke');
+
+    const appMock = {
+        get: sinon.stub()
+    };
+
+    getJoke(appMock);
+
+    const requestMock = {};
+    const responseMock = {
+        send: sinon.stub()
+    };
+    const getJokeCallback = appMock.get.getCalls()[0].args[1];
+
+    getJokeCallback(requestMock, responseMock);
+
+    t.ok(getRandomJokeStub.calledOnceWith(), 'get random joke resource called');
 
     t.end();
 });
