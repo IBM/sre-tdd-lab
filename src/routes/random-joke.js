9d0dad9baa6eaddc06de2638cc5af1c347da43b4
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-12 07:43:30 -0400

    feat(jokes): send successful response

--- src/routes/random-joke.js
+++ src/routes/random-joke.js
@@ -1,8 +1,9 @@
 const dadJokes = require('../resources/dad-jokes');
 
 const getJoke = app => {
-    app.get('/random-joke', async () => {
-        await dadJokes.getRandomJoke();
+    app.get('/random-joke', async (request, response) => {
+        const jokeResponse = await dadJokes.getRandomJoke();
+        response.send(jokeResponse);
     });
 };
 
