41f51d250f2e3f8723bbf2d4dacffebe4d9bbc1d
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-12 07:56:59 -0400

    feat(jokes): green - random jokes throws the error when it fails

--- src/routes/random-joke.js
+++ src/routes/random-joke.js
@@ -2,8 +2,12 @@ const dadJokes = require('../resources/dad-jokes');
 
 const getJoke = app => {
     app.get('/random-joke', async (request, response) => {
-        const jokeResponse = await dadJokes.getRandomJoke();
-        response.send(jokeResponse);
+        try {
+            const jokeResponse = await dadJokes.getRandomJoke();
+            response.send(jokeResponse);
+        } catch (error) {
+            throw new Error(error);
+        }
     });
 };
 
