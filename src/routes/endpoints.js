0531911ea82bed5b81723c977baa165d935f2af7
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-13 10:17:47 -0400

    fix(route): add missing 's' to search-jokes route

--- src/routes/endpoints.js
+++ src/routes/endpoints.js
@@ -1,5 +1,5 @@
 const RANDOM_JOKE_ENDPOINT = '/random-joke';
-const SEARCH_JOKE_ENDPOINT = '/search-joke';
+const SEARCH_JOKE_ENDPOINT = '/search-jokes';
 
 module.exports = {
     RANDOM_JOKE_ENDPOINT,
