7ee916dca171694d1490c1054c245de2fd25fd27
Stephen Kawaguchi <stephen.kawaguchi@ibm.com>
2020-09-13 10:07:05 -0400

    feat(routes): green - add the search term route to the route factory

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const randomJokeRoute = require('./random-joke');
+const searchJokesRoute = require('./search-jokes');
 
 const configureRoutes = app => {
     // Note that we imported like this because importing the function directly didn't allow easy mocking. This is where ES Modules might help because they we can do import * as randomJoke.
     randomJokeRoute.getJoke(app);
+    searchJokesRoute.searchJokes(app);
 };
 
 module.exports = {
--- tests/src/routes/index.test.js
+++ tests/src/routes/index.test.js
@@ -6,11 +6,14 @@ const tap = require('tap');
 const sinon = require('sinon');
 
 const randomJokeRoute = require('../../../src/routes/random-joke');
+const searchJokesRoute = require('../../../src/routes/search-jokes');
 
 const randomJokeStub = sinon.stub(randomJokeRoute, 'getJoke');
+const searchJokesStub = sinon.stub(searchJokesRoute, 'searchJokes');
 
 tap.afterEach(done => {
     randomJokeStub.reset();
+    searchJokesStub.reset();
     done();
 });
 
@@ -19,6 +22,7 @@ tap.test('configure routes', t => {
     configureRoutes(appMock);
 
     t.ok(randomJokeStub.calledWithExactly(appMock), 'the random joke route factory is passed the app');
+    t.ok(searchJokesStub.calledWithExactly(appMock), 'the search jokes route factory is passed the app');
 
     t.end();
 });
